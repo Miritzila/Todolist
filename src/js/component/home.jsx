7ab9b6c916beafe6baa094b0129d7f35f491c57a
Miriam Asencio <148205684+Miritzila@users.noreply.github.com>
2024-02-01 10:05:09 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+	const [tasks, setTasks] = useState([""]);
+	const [newTask, setNewTask] = useState("");
+
+	const handleNewTaskChange = (event) => {
+		setNewTask(event.target.value);
+	};
+
+	const addTask = () => {
+		if (newTask.trim() !== "") {
+			setTasks([...tasks, newTask]);
+			setNewTask("");
+		}
+	};
+
+	const handleKeyPress = (event) => {
+		if (event.key === "Enter") {
+			addTask();
+		}
+	};
+
+	const listItems = tasks.map((task, index) => <li key={index}>{task}</li>);
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container my-5">
+			<h1 className="text-center mb-4">Mis Tareas</h1>
+			<div className="notebook">
+				<ul className="list-group shadow">
+					{listItems}
+				</ul>
+			</div>
+			<input
+				className="form-control mt-3"
+				type="text"
+				placeholder="Añade una nueva tarea..."
+				value={newTask}
+				onChange={handleNewTaskChange}
+				onKeyPress={handleKeyPress}
+			/>
 		</div>
 	);
 };
