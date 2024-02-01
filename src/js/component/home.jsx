1d65d382b4a9bc097a372a7d42650c2d411bf930
Miriam Asencio <148205684+Miritzila@users.noreply.github.com>
2024-02-01 12:41:06 +0000

    second commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,51 +1,49 @@
 import React, { useState } from "react";
 
-
 const Home = () => {
+    const [tasks, setTasks] = useState([""]);
+    const [newTask, setNewTask] = useState("");
 
-	const [tasks, setTasks] = useState([""]);
-	const [newTask, setNewTask] = useState("");
+    const handleNewTaskChange = (event) => {
+        setNewTask(event.target.value);
+    };
 
-	const handleNewTaskChange = (event) => {
-		setNewTask(event.target.value);
-	};
+    const addTask = () => {
+        if (newTask.trim() !== "") {
+            setTasks([...tasks, newTask]);
+            setNewTask("");
+        }
+    };
 
-	const addTask = () => {
-		if (newTask.trim() !== "") {
-			setTasks([...tasks, newTask]);
-			setNewTask("");
-		}
-	};
+    const handleKeyPress = (event) => {
+        if (event.key === "Enter") {
+            addTask();
+        }
+    };
 
-	const handleKeyPress = (event) => {
-		if (event.key === "Enter") {
-			addTask();
-		}
-	};
+    const listItems = tasks.map((task, index) => (
+        <li className="list-group-item d-flex justify-content-between align-items-center" key={index}>
+            {task}
+        </li>
+    ));
 
-	const listItems = tasks.map((task, index) => <li key={index}>{task}</li>);
-
-	return (
-		<div ClassName="w-50 text-center mb-3" >
-			<div className="list-group">
-				<div className="list-group-item d-flex gap-2">
-					<input
-						className="form-control mt-3"
-						type="text"
-						placeholder="Añade una nueva tarea..."
-						value={newTask}
-						onChange={handleNewTaskChange}
-						onKeyPress={handleKeyPress}
-					/>
-				</div>
-				<div className="list-group-item d-flex gap-2">
-					<ul>
-						{listItems}
-					</ul>
-				</div>
-			</div>
-		</div>
-	);
+    return (
+        <div className="container">
+            <div className="w-50 text-center mb-3">
+            </div>
+            <ul className="list-group w-100">
+				<li><input
+                    className="form-control"
+                    type="text"
+                    placeholder="Añade una nueva tarea..."
+                    value={newTask}
+                    onChange={handleNewTaskChange}
+                    onKeyPress={handleKeyPress}
+                /></li>
+                {listItems}
+            </ul>
+        </div>
+    );
 };
 
 export default Home;
